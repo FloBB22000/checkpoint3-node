34f874e51d3dc0e35a0ba6bc816ab0f04e209d63
FloBB22000 <florence.goslino@sncf.fr>
2022-10-19 13:46:33 +0200

    checkpoint 3

--- src/api/albums/getAll.js
+++ src/api/albums/getAll.js
@@ -1,3 +1,19 @@
-module.exports = (req, res) => {
-  // your code here !
-};
+const database = require("./database");
+
+
+  const getAll = (req, res) => {
+    database
+      .query("select * from album")
+      .then(([albums]) => {
+        res.json(albums);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error retrieving data from database");
+      });
+  };
+
+
+module.exports = {
+  getAll
+}
